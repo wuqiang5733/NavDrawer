22cf232fc7fd79ce328f24d519bde95f0c857835
xiaoqianghou <2662651103@qq.com>
2017-01-07 01:02:04 +0800

    工作啦

--- app/src/main/java/org/xuxiaoxiao/navdrawer/BaseActivity.java
+++ app/src/main/java/org/xuxiaoxiao/navdrawer/BaseActivity.java
@@ -3,7 +3,10 @@ package org.xuxiaoxiao.navdrawer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +18,21 @@ public abstract class BaseActivity extends AppCompatActivity{
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.include_toolbar);
+
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(drawerLayout.isDrawerOpen(Gravity.LEFT))
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                else
+                    drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
 
         findViewById(R.id.navdrawer_inbox).setOnClickListener(new View.OnClickListener() {
             @Override
